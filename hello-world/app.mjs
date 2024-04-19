8d61f6ccc53fff2025e5d962b2b941eb4932bb62
Luis Pérez <32213774+matemiguel2005@users.noreply.github.com>
2024-04-19 11:48:57 -0600

    Corrigiendo error Throw Exception en Function HelloWorld
    Se quito el comentario y se agregaron las lineas correctamente.

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -12,14 +12,14 @@
  */
 
 export const lambdaHandler = async (event, context) => {
-  throw new Error("This will cause a deployment rollback")
-    /*const response = {
+  //throw new Error("This will cause a deployment rollback")
+    const response = {
       statusCode: 200,
       body: JSON.stringify({
         message: 'Workshop claro',
       })
     };
 
-    return response;*/
+    return response;
   };
   
